a90712f168aca0c1d533ce722c7e019a7ac81aab
tiziimusca <tizianomusca@gmail.com>
2026-05-20 23:07:58 -0300

    change sec

--- back/src/main/java/com/example/auctionapp/config/SecurityConfig.java
+++ back/src/main/java/com/example/auctionapp/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.example.auctionapp.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
@@ -13,5 +15,16 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll())
+                .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+
+        return http.build();
     }
 }
