3cf230377213ca34b892564902a1ceaa8a14cd78
tiziimusca <tizianomusca@gmail.com>
2026-06-28 01:25:28 -0300

    fix tabla personas en mysql

--- back/src/main/java/com/example/auctionapp/model/Persona.java
+++ back/src/main/java/com/example/auctionapp/model/Persona.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -31,6 +32,7 @@ public class Persona {
     @Column(length = 15)
     private String estado;
 
-    @Column(columnDefinition = "varbinary(max)")
+    @Lob
+    @Column
     private byte[] foto;
 }
