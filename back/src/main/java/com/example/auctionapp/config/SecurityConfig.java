31da35d2afb93ade7a9f2dddf73ff869765ff20a
tiziimusca <tizianomusca@gmail.com>
2026-07-04 15:53:46 -0300

    fix CORS

--- back/src/main/java/com/example/auctionapp/config/SecurityConfig.java
+++ back/src/main/java/com/example/auctionapp/config/SecurityConfig.java
@@ -25,12 +25,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:8081",
-                "http://127.0.0.1:8081",
-                "http://localhost:19006",
-                "http://127.0.0.1:19006"
-        ));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
